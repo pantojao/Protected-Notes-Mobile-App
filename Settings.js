c875935895e524619ff9ea87fc96431b553674a6
pantojao <61061684+pantojao@users.noreply.github.com>
2021-05-12 07:52:13 -0700

    added password reset

--- Settings.js
+++ Settings.js
@@ -1,40 +1,42 @@
 import React, { useState, useMemo, useEffect } from "react";
-import { ToggleButton, Switch, Button } from "react-native-paper";
+import { ToggleButton, Switch, Button, Portal, Dialog, Paragraph } from "react-native-paper";
 import { Text, View } from "react-native";
-import { UserNotes } from "./UserNotes";
 import * as firebase from "firebase";
 
 const Settings = () => {
-	const [sessionPersistance, setSessionPersistence] = useState(false);
-    
+	const [visible, setVisible] = useState(false);
 
-	useEffect(() => {
-		console.log(firebase.auth.Auth.Persistence.NONE);
-	}, []);
-
-	const changePersistance = async () => {
-		setSessionPersistence(!sessionPersistance);
-
-		if (setSessionPersistence) {
-			await firebase.auth().setPersistance(firebase.auth.Auth.Persistence.NONE);
-		} else {
-			await firebase.auth().setPersistance(firebase.auth.Auth.Persistence.SESSION);
-		}
+	const changePassword = async () => {
+		const auth = firebase.auth();
+		const emailAddress = auth.currentUser.email;
+		
+		setVisible(true);
+		await auth.sendPasswordResetEmail(emailAddress);
 	};
-
-	const changePassword = () => {
-
-    };
 
 	const logout = async () => {
 		await firebase.auth().signOut();
 	};
 
 	return (
-		<View>
-			<Button onPress={changePassword}>Change Password</Button>
-			<Button onPress={logout}>Logout</Button>
-		</View>
+		<>
+			<Portal>
+				<Dialog visible={visible} onDismiss={() => setVisible(false)}>
+					<Dialog.Content>
+						<Paragraph>A password reset has been sent to your email.</Paragraph>
+					</Dialog.Content>
+					<Dialog.Actions>
+						<Button onPress={() => setVisible(false)}>Done</Button>
+					</Dialog.Actions>
+				</Dialog>
+			</Portal>
+			<View style={{ justifyContent: "space-evenly", height: "90%" }}>
+				<Button onPress={changePassword}>Change Password</Button>
+				<Button onPress={logout} color="red" mode="contained" style={{ width: 100, alignSelf: "center" }}>
+					Logout
+				</Button>
+			</View>
+		</>
 	);
 };
 
